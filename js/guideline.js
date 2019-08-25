5b3c214672dae3f9ce118d3658c1d36190ba4b23
Yi Zhang <cattyzhang0110@gmail.com>
2019-08-26 01:27:48 +1000

    Iteration 1 v1.0

---
+++ js/guideline.js
@@ -0,0 +1,14 @@
+function showBoatGuideline(id){
+    var id_text = id + '_text'
+    document.getElementById(id_text).innerHTML = '<img src="img/boat_icon/'+ id_text +'.png"/>'
+}
+
+function showRiverGuideline(id){
+    var id_text = id + '_text'
+    document.getElementById(id_text).innerHTML = '<img src="img/river_icon/'+ id_text +'.png"/>'
+}
+
+function showPoolGuideline(id){
+    var id_text = id + '_text'
+    document.getElementById(id_text).innerHTML = '<img src="img/pool_icon/'+ id_text +'.png"/>'
+}
