7c34ca3a21b7e2120d19b0382bc7bbfa74d5c95a
Yi Zhang <cattyzhang0110@gmail.com>
2019-08-27 21:33:36 +1000

    Iteration 1 v2.0

--- js/guideline.js
+++ js/guideline.js
@@ -12,3 +12,8 @@ function showPoolGuideline(id){
     var id_text = id + '_text'
     document.getElementById(id_text).innerHTML = '<img src="img/pool_icon/'+ id_text +'.png"/>'
 }
+
+function showBeachGuideline(id){
+    var id_text = id + '_text'
+    document.getElementById(id_text).innerHTML = '<img src="img/beach_icon/'+ id_text +'.png"/>'
+}
